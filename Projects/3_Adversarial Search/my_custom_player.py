6a5c1b5aeeaa3c4635b3dd6b9c33e27347adc261
psnx <panyitamas@gmail.com>
2021-08-23 20:13:30 +0200

    WIP: We manage to loose with Monte Carlo

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,10 +1,108 @@
 
+from collections import defaultdict
+from os import POSIX_FADV_DONTNEED
+from pickle import READONLY_BUFFER
+from typing import DefaultDict
 from isolation.isolation import S, Isolation
 from sample_players import DataPlayer
 
 #TODO: Remove this import for submission
 from isolation import DebugState
+import random
+import math
 
+class Node:
+    def __init__(self, state) -> None:
+        self.children = set()
+        self.parent = None
+        self.visit_count = 0
+
+    def is_terminal(self, state):
+        return state.terminal_test()
+
+    def reward(self, state):
+        return state.utility()
+
+class MCTS():
+    def __init__(self, player_id) -> None:
+        self.player_id = player_id
+        self.q = defaultdict(int)
+        self.n = defaultdict(int)
+        self.children = dict() # child relationships
+        self.exploration_weight = 1
+        self.parent = None # dict when not root
+        self.rewards = dict()
+
+    def unvisited_children(self, state):
+        self.children[state] = [(state.result(action)) for action in state.actions()]
+        return [a for a in self.children[state] if self.n[a] == 0]
+    
+    def ucb(self, state):
+        c = self.exploration_weight # to be tweaked?
+        if not self.parent : return float("inf")
+        return (self.q[state] + c * math.sqrt(2 * math.log(self.parent[state]) / self.n[state]))
+
+    # this may be is not efficient...
+    def find_children(self, state):
+        if state.termnal_test():
+            return None # we are at a leaf
+        children = []
+        for a in state.actions():
+            result = state.result(a)
+            self.parent[result] = state 
+            children.append(result)                
+        self.children[state] = children
+        return children
+
+    def random_child(self, state):
+        random.choice([state.result(a) for a in state.actions()])
+
+    def reward(self, state):
+        if state.terminal_test():
+            return state.utility(state.player_is)
+        print("we should not have been here")
+        return 0
+
+    def best_child(self, state):
+        #Ranking children by policy 
+        return max(self.children[state], key=self.ucb)  
+
+    def expand(self, state):
+        new_state = random.choice(self.unvisited_children(state))
+        self.children[new_state] = new_state
+        return new_state
+
+        
+    def move(self, state):
+        for c in state.actions():
+            if state.result(c).terminal_test():
+                if state.result(c).utility(self.player_id) == float("inf"):
+                    return c
+
+    def advance(self, state):
+        while not state.terminal_test():
+            if self.unvisited_children(state):
+                return self.expand(state)
+            state = self.best_child(state)
+            return state
+
+    def backpropagate(self, state):   
+        r = state.utility(self.player_id)
+        while state is not None:
+            self.n[state] += 1
+            #self.q[state] = ((node.n - 1)/node.n) * node.q + 1/node.n * r
+            self.q[state] = ((self.n[state]-1) / self.n[state]) + 1/self.n[state] * r
+            print(f"backpop: {state=} ")
+            state = self.parent[state] if self.parent else None
+
+    def main(self, state):
+        for _ in range(100):
+            next_state = self.advance(state)
+            self.rewards[next_state] = self.ucb(next_state)
+            self.backpropagate(state)
+        #node should be root now... TODO: check
+        return max([n for n in self.children[next_state]], key=lambda n: self.q[n])
+                
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
 
@@ -22,62 +120,7 @@ class CustomPlayer(DataPlayer):
       any pickleable object to the self.context attribute.
     **********************************************************************
     """
-    def score(self, state):
-        own_loc = state.locs[self.player_id]
-        opp_loc = state.locs[1 - self.player_id]
-        own_liberties = state.liberties(own_loc)
-        opp_liberties = state.liberties(opp_loc)
-        #own_xy = DebugState.ind2xy(own_loc)
-        
-        #(num_of_my_moves - 2 * num_of_opponent_moves) + (num_of_blank_spaces / 2 + 2 * num_of_my_moves) / 13
-   
-
-        return len(own_liberties) - len(opp_liberties)
-
-    def max_value(self, state, depth, alpha, beta):
-        """ Return the game state utility if the game is over,
-        otherwise return the maximum value over all legal successors
-        """        
-        if state.terminal_test():
-            return state.utility(self.player_id)
-        if depth <=0 : return self.score(state)
-        v = float("-inf")
-        for a in state.actions():
-            v = max(v, self.min_value(state.result(a), depth - 1, alpha, beta))
-            if v >= beta : return v
-            alpha = max(alpha, v)
-        return v
-
-    def min_value(self, state, depth, alpha, beta):
-        """ Return the game state utility if the game is over,
-        otherwise return the minimum value over all legal successors
-        """
-        if state.terminal_test():
-            return state.utility(self.player_id)
-        if depth <=0 : return self.score(state)
-        v = float("inf")
-        for a in state.actions():
-            v = min(v, self.max_value(state.result(a), depth - 1, alpha, beta))
-            if v <= alpha : return v
-            beta = min(beta, v)
-        return v
-
-    #def minimax(self, state, depth):
-    #    return max(state.actions(), key=lambda s: self.min_value(state.result(s), depth-1))
-
-    def alpha_beta(self, state, depth):        
-        alpha = float("-inf")
-        beta = float("inf")  
-        best_score = float("-inf")
-        best_move = None      
-
-        for a in state.actions():
-            v = self.min_value(state.result(a), depth, alpha, beta)
-            alpha = max(alpha, v)
-            if v > best_score:
-                best_score = v
-                best_move = a 
-        return best_move        
+      
 
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
@@ -112,4 +155,8 @@ class CustomPlayer(DataPlayer):
         #debug_board = DebugState.from_state(state)
         #print(debug_board)
         #print(state.ply_count)
-        self.queue.put(self.alpha_beta(state, 2))
+        #move = (max(state.actions(), key=lambda n: self.score(n)))
+        tree = MCTS(self.player_id)
+        move = tree.main(state)
+        print(f"next move {move=}")
+        self.queue.put(move)
